9657c3c30bf1e0c4b04c7d9ca832505bd9514414
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-25 14:52:20 -0300

    Requisito 1 feito.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;

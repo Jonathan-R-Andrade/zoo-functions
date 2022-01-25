e85477e18de5b3339f5ce508ed2a73c5c0e8f1c3
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-25 20:28:23 -0300

    Requisito 7 feito.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+function getResidentsName(residents, sex, sorted) {
+  const residentsName = [];
+  residents.forEach((resident) => {
+    if (!sex || resident.sex === sex) {
+      residentsName.push(resident.name);
+    }
+  });
+  if (sorted) residentsName.sort();
+  return residentsName;
+}
+
+function getAnimals(withName, sex, sorted) {
+  const animals = {};
+  data.species.forEach((specie) => {
+    if (!animals[specie.location]) animals[specie.location] = [];
+    if (withName) {
+      animals[specie.location].push({
+        [specie.name]: getResidentsName(specie.residents, sex, sorted),
+      });
+    } else {
+      animals[specie.location].push(specie.name);
+    }
+  });
+  return animals;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  return (!options || !options.includeNames)
+    ? getAnimals(false)
+    : getAnimals(true, options.sex, options.sorted);
 }
 
 module.exports = getAnimalMap;

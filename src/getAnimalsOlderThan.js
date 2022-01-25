6c7624b4d586e5401513db12150c1871f0135517
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-25 15:06:43 -0300

    Requisito 2 feito.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+  if (animals) {
+    return animals.residents.every((resident) => resident.age >= age);
+  }
+  return undefined;
 }
 
 module.exports = getAnimalsOlderThan;

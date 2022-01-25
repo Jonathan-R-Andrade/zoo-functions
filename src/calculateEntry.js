5219d349a37674c3b827f8adc28f1166e6314468
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-25 17:57:57 -0300

    Requisito 6 feito.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const classifiedAges = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach(({ age }) => {
+    if (age < 18) classifiedAges.child += 1;
+    else if (age >= 50) classifiedAges.senior += 1;
+    else classifiedAges.adult += 1;
+  });
+  return classifiedAges;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) return 0;
+  const classifiedAges = countEntrants(entrants);
+  let pay = data.prices.child * classifiedAges.child;
+  pay += data.prices.adult * classifiedAges.adult;
+  pay += data.prices.senior * classifiedAges.senior;
+  return pay;
 }
 
 module.exports = { calculateEntry, countEntrants };

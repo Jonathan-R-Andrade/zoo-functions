588cf80651b78b3fa9dc6b63b1760343f3342f2b
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-25 16:54:35 -0300

    Requisito 5 feito.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function getTotalAnimals() {
+  const animals = {};
+  data.species.forEach((specie) => {
+    animals[specie.name] = specie.residents.length;
+  });
+  return animals;
+}
+
+function countAnimalsBySex({ residents } = {}, sex) {
+  if (residents) {
+    return residents.reduce((total, animal) => (
+      (!sex || animal.sex === sex) ? total + 1 : total
+    ), 0);
+  }
+  return 0;
+}
+
+function countAnimals({ specie, sex } = {}) {
+  if (!specie && !sex) return getTotalAnimals();
+  const animals = data.species.find((animal) => animal.name === specie);
+  return countAnimalsBySex(animals, sex);
 }
 
 module.exports = countAnimals;

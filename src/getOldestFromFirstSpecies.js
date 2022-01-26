04a0c278cad613a9be82c4ca8572360fff5ee01a
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-26 01:34:04 -0300

    Requisito 9 feito.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const getSpeciesByIds = require('./getSpeciesByIds');
+
+function getOldestAnimal(residents) {
+  return residents.reduce((oldest, animal) => ((animal.age > oldest.age) ? animal : oldest));
+}
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((wantedEmployee) => wantedEmployee.id === id);
+  const firstSpecies = getSpeciesByIds(employee.responsibleFor[0])[0];
+  const { name, sex, age } = getOldestAnimal(firstSpecies.residents);
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
